51dbb17fb80eddbfdd92475e11e7945598113786
rupeshthapa123 <rupesh.thapa2050@gmail.com>
2021-09-19 17:08:58 +0545

    category, payment, account function added

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
+import store from '../store'
+
 import Home from '../views/Home.vue'
 import Product from '../views/Product.vue'
+import Category from '../views/Category.vue'
+import Search from '../views/Search.vue'
+import Cart from '../views/Cart.vue'
+import SignUp from '../views/SignUp.vue'
+import Login from '../views/Login.vue'
+import MyAccount from '../views/MyAccount.vue'
+import Checkout from '../views/Checkout.vue'
+import Success from '../views/Success.vue' 
 
 const routes = [
   {
@@ -17,10 +28,56 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
-    path: '/:category_slug/:product_slug/',
+    path: '/Sign-up',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/Log-in',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/my-account',
+    name: 'MyAccount',
+    component: MyAccount,
+    meta: {
+      requireLogin: true
+    }
+  },
+  {
+    path: '/search',
+    name: 'Search',
+    component: Search
+  },
+  {
+    path: '/Cart',
+    name: 'Cart',
+    component: Cart
+  },
+  {
+    path: '/cart/success',
+    name: 'Success',
+    component: Success
+  },
+  {
+    path: '/cart/checkout',
+    name: 'Checkout',
+    component: Checkout,
+    meta: {
+      requireLogin: true
+    }
+  }, 
+  {
+    path: '/:category_slug/:product_slug',
     name: 'Product',
     component: Product
-  }
+  },
+  {
+    path: '/:category_slug',
+    name: 'Category',
+    component: Category
+  },
 ]
 
 const router = createRouter({
@@ -28,4 +85,12 @@ const router = createRouter({
   routes
 })
 
+router.beforeEach((to, from, next)=> {
+  if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+    next({ name:'Login', query: {to: to.path } });
+  } 
+  else {
+    next()
+  }
+})
 export default router
--- src/views/Product.vue
+++ src/views/Product.vue
@@ -22,7 +22,7 @@
           </div>
 
           <div class="control">
-            <a class="button is-dark">Add to cart</a>
+            <a class="button is-dark" @click="addToCart">Add to cart</a>
           </div>
         </div>
       </div>
@@ -31,7 +31,8 @@
 </template>
 
 <script>
-import axios from "axios";
+import axios from "axios"
+import { toast } from 'bulma-toast'
 
 export default {
   name: "Product",
@@ -45,19 +46,46 @@ export default {
     this.getProduct();
   },
   methods: {
-    getProduct() {
+    async getProduct() {
+      this.$store.commit('setIsLoading', true) 
+
       const category_slug = this.$route.params.category_slug;
       const product_slug = this.$route.params.product_slug;
 
-      axios
+      await axios
         .get(`/api/v1/products/${category_slug}/${product_slug}`)
         .then((response) => {
           this.product = response.data;
+
+          document.title = this.product.name + ' | Djackets'
         })
         .catch((error) => {
           console.log(error);
         });
+
+      this.$store.commit('setIsLoading', false)
     },
-  },
+    addToCart() {
+      if (isNaN(this.quantity) || this.quantity < 1) {
+        this.quantity = 1
+      }
+
+      const item = {
+        product: this.product,
+        quantity: this.quantity
+      }
+
+      this.$store.commit('addToCart', item)
+
+      toast({
+        message: 'The Product was added to the Cart',
+        type: 'is-success',
+        dismissible: true,
+        pauseOnHover: true,
+        duration: 2000,
+        position: 'bottom-right',
+      })
+    }
+  }
 };
 </script>
